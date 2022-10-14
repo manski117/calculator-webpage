910bbb2f60e6d336c5feed2047b99ef5ccf7774c
krmanski <choromanski117@gmail.com>
2022-10-14 14:53:06 -0400

    Can now display and clear
    Wrote functions that can successfully add strings to the display,
    overwrite strings on the display, and can clear JUST the display
    but no other memory items.

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("js linked successfully")
+
+//Code below sends stuff to display
+function displayThisThing (string){
+    //replaces the whole readout with whatever the passed in string is
+    let display = document.querySelector("#display");
+    display.style.color = "red";
+    display.innerText = string;
+  }
+  
+  function addToDisplay (string){
+    //adds the passed in string to the end of the display readout
+    let display = document.querySelector("#display");
+    //get the current string that is displayed with .innerText
+    let currentDisplay = display.innerText;
+    display.style.color = "red";
+    //add the new string to the old string
+    display.innerText = currentDisplay + " " + string;
+  
+  }
+  
+  function clearDisplay(){
+    //replace existing text with a blank string
+    let display = document.querySelector("#display");
+    let currentDisplay = display.innerText = "";
+  }
+  
